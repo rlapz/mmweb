2e1e4721d9dd9b526d22bf6941a5c24a76aace5c
Arthur Lapz <arthurlapz@gmail.com>
2026-02-08 13:52:24 +0700

    model: simplify table layouts
    Signed-off-by: Arthur Lapz <arthurlapz@gmail.com>

--- model/user.go
+++ model/user.go
@@ -5,21 +5,13 @@ import (
 )
 
 type User struct {
-	Id        int32     `json:"id"`
-	Name      string    `json:"name"`
-	Flags     int32     `json:"flags"`
-	CreatedAt time.Time `json:"created_at"`
-	CreatedBy int64     `json:"created_by"`
-}
-
-type UserDetail struct {
 	Id        int32  `json:"id"`
-	IdUser    int32  `json:"id_user"`
+	Name      string `json:"name"`
 	FirstName string `json:"first_name"`
 	LastName  string `json:"last_name"`
 	Email     string `json:"email"`
 	Password  []byte
-
+	Flags     int32      `json:"flags"`
 	CreatedAt time.Time  `json:"created_at"`
 	CreatedBy int64      `json:"created_by"`
 	UpdatedAt *time.Time `json:"updated_at"`
--- repo/sqlite/query/user.go
+++ repo/sqlite/query/user.go
@@ -1,12 +1,7 @@
 package query
 
 const SelectUserPasswordByName = `
-	SELECT b.password
-	FROM t_user AS a
-	JOIN t_user_detail AS b
-		ON (b.id_user = a.id)
-	WHERE (a.name = ?)
-	ORDER BY b.id DESC
-	LIMIT 1;
-
+	SELECT password
+	FROM t_user
+	WHERE (name = ?)
 `
